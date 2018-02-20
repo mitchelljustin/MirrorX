ab0e9b705fd7ece8749c42918aea7e82c374957e
Mitchell Van Der Hoeff <mitchell.vanderhoeff@gmail.com>
2018-02-20 10:25:53 -0500

    Stylin

--- www/src/util/web3.js
+++ www/src/util/web3.js
@@ -2,10 +2,10 @@ import Web3 from 'web3'
 
 let web3 = null
 if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
-  console.log("METAMASK DETECTED")
+  console.log('METAMASK DETECTED')
   web3 = new Web3(window.web3.currentProvider)
 } else {
-  console.log("WARNING: NO METAMASK DETECTED")
+  console.log('WARNING: NO METAMASK DETECTED')
   web3 = {}
 }
 
